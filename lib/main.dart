b38f649762e369e2235725ed0561371c42ce64c8
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-01-28 20:25:12 +0200

    add bloc provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:note_app/presentation/note/note_screen.dart';
 import 'package:note_app/presentation/resources/string_manager.dart';
-
-void main() async{
-
+// ignore: depend_on_referenced_packages
+import 'package:flutter_bloc/flutter_bloc.dart';
+void main() async {
   await Hive.initFlutter();
   await Hive.openBox(StringManager.notesBox);
   Hive.registerAdapter(NoteModelAdapter());
@@ -19,13 +20,18 @@ class NoteApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: const Color(0xff303030),
+    return MultiBlocProvider(
+      providers : [
+        BlocProvider(create:(context)=>AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: const Color(0xff303030),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const NoteScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const NoteScreen(),
     );
   }
 }
