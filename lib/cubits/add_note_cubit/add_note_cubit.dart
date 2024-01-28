197273b34ccb504864d2fd7d48a31508572d87e9
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-01-28 19:39:17 +0200

    add add_note_cubit

---
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,8 @@
+// ignore: depend_on_referenced_packages
+import 'package:bloc/bloc.dart';
+
+part 'add_note_state.dart';
+
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteInitial());
+}
