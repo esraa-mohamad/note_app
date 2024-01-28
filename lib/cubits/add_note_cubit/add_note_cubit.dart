37b3f77f31a68501cc3665fe0a771bcb97e327a9
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-01-28 20:12:06 +0200

    add note method in add_note_cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,27 @@
 // ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:note_app/models/note_model.dart';
+import 'package:note_app/presentation/resources/string_manager.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+
+
+  addNote(NoteModel noteModel) async
+  {
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(StringManager.notesBox);
+
+      emit(AddNoteSuccess());
+
+      await notesBox.add(noteModel);
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/presentation/note/note_screen.dart';
 import 'package:note_app/presentation/resources/string_manager.dart';
 
@@ -7,6 +8,7 @@ void main() async{
 
   await Hive.initFlutter();
   await Hive.openBox(StringManager.notesBox);
+  Hive.registerAdapter(NoteModelAdapter());
 
   runApp(const NoteApp());
 }
