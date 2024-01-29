5af810a42d0264034787bbd702778f87a3c39db2
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-01-29 16:21:47 +0200

    add cubit to bottom sheet

--- lib/modules/add_note_bottom_sheet.dart
+++ lib/modules/add_note_bottom_sheet.dart
@@ -1,14 +1,44 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 
 import 'add_note_form.dart';
 
-class AddNoteBottomSheet extends StatelessWidget {
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AddNoteBottomSheet extends StatefulWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
+  State<AddNoteBottomSheet> createState() => _AddNoteBottomSheetState();
+}
+
+class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
+
+  @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: AddNoteForm(),
+    return  SingleChildScrollView(
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if(state is AddNoteFailure){
+            if (kDebugMode) {
+              print('Failed  ${state.errorMessage}');
+            }
+          }
+          if(state is AddNoteSuccess){
+            Navigator.pop(context);
+          }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false ,
+              child: const AddNoteForm()
+          );
+        },
+      ),
     );
   }
 }
