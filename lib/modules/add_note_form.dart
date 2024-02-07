a8c0adad7b9c604ebe954fa90c7a72d0982cbea6
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-02-07 16:58:29 +0200

    color list view part one

--- lib/modules/add_note_form.dart
+++ lib/modules/add_note_form.dart
@@ -5,6 +5,7 @@ import 'package:intl/intl.dart';
 import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/modules/color_list_view.dart';
 import 'custom_button.dart';
 import 'custom_text_field.dart';
 
@@ -49,6 +50,10 @@ class _AddNoteFormState extends State<AddNoteForm> {
               subTitle = value;
             },
           ),
+          const SizedBox(
+            height: 16,
+          ),
+          const ColorListView(),
           const SizedBox(
             height: 80,
           ),
---
+++ lib/modules/color_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:note_app/modules/color_item.dart';
+
+class ColorListView extends StatelessWidget {
+  const ColorListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 38*2,
+      child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context,index){
+            return const ColorItem();
+          }
+      ),
+    );
+  }
+}
